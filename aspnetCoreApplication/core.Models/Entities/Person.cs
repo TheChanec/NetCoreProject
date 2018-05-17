05d9d53921ffa9cbbfd6ef811f29a53be21814c7
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-05-17 13:19:00 -0500

    Persona create

---
+++ aspnetCoreApplication/core.Models/Entities/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace core.Models.Entities
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthday { get; set; }
+
+    }
+}
