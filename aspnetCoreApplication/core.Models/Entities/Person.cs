7451795ef204bf87d5c89cddf73969e9fd9f8a3b
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-05-17 13:49:44 -0500

    Entidad Persona

--- aspnetCoreApplication/core.Models/Entities/Person.cs
+++ aspnetCoreApplication/core.Models/Entities/Person.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace core.Models.Entities
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class Person
     {
         public int Id { get; set; }
@@ -11,6 +14,30 @@ namespace core.Models.Entities
         public string MiddleName { get; set; }
         public string LastName { get; set; }
         public DateTime Birthday { get; set; }
+        public int Age
+        {
+            get
+            {
+                if (Birthday != null)
+                {
+                    DateTime now = DateTime.Today;
+                    int age = now.Year - Birthday.Year;
+                    if (now < Birthday.AddYears(age)) age--;
+
+                    return age;
+                }
+                else {
+                    return 0;
+                }
+            }
+        }
+        public string FullName
+        {
+            get
+            {
+                return Name + " " + MiddleName + " " + LastName;
+            }
+        }
 
     }
 }
