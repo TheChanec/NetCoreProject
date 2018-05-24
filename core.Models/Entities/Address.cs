41de8f8ef534bcad9ae27ab94c186a2d8d58dc7a
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-05-24 10:28:03 -0500

    Create Address

---
+++ core.Models/Entities/Address.cs
@@ -0,0 +1,14 @@
+﻿using core.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Models.Entities
+{
+    public class Address
+    {
+        public int Id { get; set; }
+
+        public Person PersonInThisAddress { get; set; }
+    }
+}
