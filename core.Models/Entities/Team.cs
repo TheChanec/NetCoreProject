8b1dfaff09d0717718cbf2458c602e4cf53d4d19
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-05-24 10:27:20 -0500

    Create Class Team

---
+++ core.Models/Entities/Team.cs
@@ -0,0 +1,22 @@
+﻿using core.Models.Entities;
+
+using System.Collections.Generic;
+
+
+namespace Core.Models.Entities
+{
+    public class Team
+    {
+
+        #region propierities
+        public int Id { get; set; }
+        public string Name { get; set; }
+        #endregion
+
+        #region navegations
+
+        public ICollection<Person> Members { get; set; }
+        
+        #endregion
+    }
+}
