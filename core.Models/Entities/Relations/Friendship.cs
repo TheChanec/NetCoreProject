28d46b32e4bb1427804a9519a7a14e6670100e9f
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-06-01 15:33:53 -0500

    cambios al momento

--- core.Models/Entities/Relations/Friendship.cs
+++ core.Models/Entities/Relations/Friendship.cs
@@ -6,6 +6,26 @@ namespace Core.Models.Entities.Relations
 {
     public class Friendship
     {
+        private int _id;
+        private string _idRequesting;
+        private string _idReceiving;
+        private DateTime _date;
+        private bool _active;
+        private ApplicationUser requesting;
+        private ApplicationUser receiving;
 
+
+
+        public int Id { get => _id; set => _id = value; }
+        public string IdRequesting { get => _idRequesting; set => _idRequesting = value; }
+        public string IdReceiving { get => _idReceiving; set => _idReceiving = value; }
+        public DateTime Date { get => _date; set => _date = value; }
+        public bool Active { get => _active; set => _active = value; }
+
+
+
+
+        public ApplicationUser Requesting { get => requesting; set => requesting = value; }
+        public ApplicationUser Receiving { get => receiving; set => receiving = value; }    
     }
 }
---
+++ core.Models/Entities/School.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Models.Entities
+{
+    public class School
+    {
+        private string _name;
+        private string _slogan;
+        private DateTime _beging;
+        private Address _adress;
+
+
+        private ICollection<ApplicationUser> _students;
+        private ICollection<ApplicationUser> _workers;
+        
+
+
+
+
+        public School()
+        {
+
+
+        }
+
+        public School(string name, string slogan, DateTime beging)
+        {
+            _name = name;
+            _slogan = slogan;
+            _beging = beging;
+        }
+    }
+}
