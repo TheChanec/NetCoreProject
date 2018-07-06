9700e67a8e7b08b9eb0d24681dfe5210d53310ca
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-07-06 11:20:31 -0500

    Cambio de nombre de la solucion y del proyecto

---
+++ core.Models/Entities/Machine.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Core.Models.Entities;
+
+namespace Core.Models
+{
+    public class Machine
+    {
+        public Machine(int id, ICollection<Mecanico> mecanicos)
+        {
+            Id = id;
+            Mecanicos = mecanicos;
+        }
+
+
+        public int Id { get; set; }
+
+        public ICollection<Mecanico> Mecanicos { get; set; }
+    }
+}
